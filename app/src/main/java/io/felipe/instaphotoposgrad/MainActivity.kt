229a1e109f1e81a7158dbc9fe50b6a8659bdbbe7
Felipe na Uninovafapi <estou.na@uninovap1.com>
2017-11-19 08:42:54 -0300

    Câmera implementada

--- app/src/main/java/io/felipe/instaphotoposgrad/MainActivity.kt
+++ app/src/main/java/io/felipe/instaphotoposgrad/MainActivity.kt
@@ -1,14 +1,19 @@
 package io.felipe.instaphotoposgrad
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CAMERA_INTENT: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,18 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CAMERA_INTENT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA_INTENT && resultCode == RESULT_OK) {
+            var extras: Bundle = data!!.getExtras()
+            var imageBitmap: Bitmap = extras.get("data") as Bitmap
+            imgv_photo.setImageBitmap(imageBitmap)
+        } else {
+            Toast.makeText(this@MainActivity, "Cancelado", Toast.LENGTH_LONG).show()
         }
     }
 
